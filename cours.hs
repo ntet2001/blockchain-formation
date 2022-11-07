e0bb7503c9b143d72d41512e549f7be3c216cf78
ntet2001 <manyamaigor2001@gmail.com>
2022-11-07 14:48:43 +0100

    cours.hs added and exo7.txt test file added for exercise 7

---
+++ cours.hs
@@ -0,0 +1,18 @@
+--exercise on Type Class
+data Color = Red | Green | Yellow 
+
+instance Show Color where
+    show Red = "Red"
+
+            {-Here is my class YesNo-}
+class YesNo a where
+    yesno :: a -> Bool
+
+--here is the instance of my class
+instance YesNo String  where
+    yesno "" = False
+    yesno _  = True
+
+
+
+
