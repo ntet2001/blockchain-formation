e84b325cb6c224febfcc348588f8ef8dff2850ef
ntet2001 <manyamaigor2001@gmail.com>
2022-11-16 10:56:42 +0100

    conference module started

---
+++ gestionConference/src/conference/Conference.hs
@@ -0,0 +1,82 @@
+module Conference
+(
+    newConference,
+    firstLetter
+    --,listConference
+    --,addSponsor
+    --,listSponsor
+    ,Conference
+    ,Sponsor
+    ,Nom
+    ,Lieu
+    ,Annee
+    ,Dated
+    ,Datef
+    ,Montant
+)where
+
+import Data.Char
+
+    {-=============== Here are my types statements ==================-}
+type Sponsor = String
+type Nom = String
+type Lieu = String
+type Annee = Int
+type Dated = String
+type Datef = String
+type Montant = Int
+data Conference = Conference {
+    idConference :: String,
+    nomConference :: String,
+    lieuConference :: String,
+    anneeConference :: Int,
+    dateDebut :: String,
+    dateFin :: String,
+    montant :: Int,
+    sponsors :: [String]
+} deriving (Show , Read, Ord) 
+
+instance Eq Conference where
+    (==) c1 c2 = nomConference c1 == nomConference c2 && anneeConference c1 == anneeConference c2
+
+
+     {-=============== Here are my functions statements ==================-}
+
+     --function that take the name and give the acronym
+firstLetter :: String -> String
+firstLetter nom = let new = words nom
+    in foldr (\(x:xs) acc -> toUpper x : acc) acc new
+        where acc = []
+
+    --function that verified the date format
+
+verifDate:: String -> Bool
+verifDate chaine 
+    | tailleChaine == 5 = f chaine
+    | otherwise = False
+        where tailleChaine = length chaine
+              f :: String -> Bool
+              f [v,w,x,y,z] = isDigit v  && isDigit w && x == '/' && isDigit y  && isDigit z 
+
+     -- function to create a new Conference object
+newConference :: Nom -> Lieu -> Annee -> Dated -> Datef -> Montant -> IO ()
+newConference nom lieu annee dated datef montanttmp = do
+    let conferencetmp = Conference {
+        idConference = firstLetter nom ++ show annee,
+        nomConference = nom,
+        lieuConference = lieu,
+        anneeConference = annee,
+        dateDebut = if verifDate dated then dated else "01/01",
+        dateFin = if verifDate datef then datef else "01/01",
+        montant = montanttmp,
+        sponsors = []
+    }
+        conference = idConference conferencetmp ++ " " ++ nomConference conferencetmp ++ " " ++ lieuConference conferencetmp ++ " " ++ show (anneeConference conferencetmp) ++" " ++ dateDebut conferencetmp ++ " " ++ dateFin conferencetmp ++ " " ++ show (montant conferencetmp)
+    appendFile "Conference.txt" conference
+    putStrLn conference
+
+    -- function to list a Conference in order of name and year
+
+
+    
+
