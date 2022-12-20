882c882967795f7f6b10cb1e1939c66672638211
ntet2001 <manyamaigor2001@gmail.com>
2022-12-20 19:05:37 +0100

    ParserCombinator.hs updated (adding instance Monad and Alternative)

--- ParserCombinator.hs
+++ ParserCombinator.hs
@@ -1,11 +1,14 @@
+
 module ParserCombinator (
 
 )where
+import Control.Applicative
 
 {-========== writing Parser from scratch ==========-}
 --Parser definition
 --type Parser a = String -> Maybe (a,String)
 newtype Parser a = Parser {getParser :: String -> Maybe (a,String)}
+
 
 --instance Functor 
 instance Functor Parser where
@@ -24,16 +27,28 @@ instance Applicative Parser where
         )
 
 --instance Monad
+instance Monad Parser where
+    (Parser f) >>= g = Parser (\xs -> do
+        (a,xs') <- f xs
+        getParser (g a) xs'
+        )
+
+
+--instance Alternative
+instance Alternative Parser where
+    empty = Parser $ const Nothing
+    (Parser a) <|> (Parser b) = Parser (\xs -> do
+            case a xs of 
+                Just x -> Just x  
+                Nothing -> b xs      
+        )
+    
 
 
 --implementation of a Parser
-char :: Char -> Parser Char
-char chaine = Parser phrase 
-    where phrase [] = Nothing
-          phrase (x:xs) 
-            | x == chaine = Just (chaine, xs)
-            | otherwise = Nothing
-
-main :: IO()
-main = do
-    putStrLn "hello  world"
+-- char :: Char -> Parser Char
+-- char chaine = Parser phrase 
+--     where phrase [] = Nothing
+--           phrase (x:xs) 
+--             | x == chaine = Just (chaine, xs)
+--             | otherwise = Nothing
