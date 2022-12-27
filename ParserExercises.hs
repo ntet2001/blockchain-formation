615180a0ea689ce9c99aa05ee61ac9c05783f6f4
ntet2001 <manyamaigor2001@gmail.com>
2022-12-27 13:03:21 +0100

    Parser Exercises modified (Parsing Uba account message)

--- ParserExercises.hs
+++ ParserExercises.hs
@@ -9,6 +9,7 @@ import qualified Text.ParserCombinators.Parsec.Token as P
 import qualified Text.Parsec as TP
 import Text.ParserCombinators.Parsec.Language
 import Data.List ( intercalate )
+import Text.Parsec (endOfLine)
 --import Text.ParserCombinators.Parsec
 --import qualified Text.ParserCombinators.Parsec.Token as P
 --import Text.ParserCombinators.Parsec.Language
@@ -40,7 +41,7 @@ parserDeDate :: Parser Date
 parserDeDate = do
         jj <- many digit 
         char '/' <|> char '-'
-        mm <- many digit 
+        mm <- many digit
         char '/' <|> char '-'
         aaaa <- many digit
         let year = read aaaa :: Int
@@ -75,8 +76,27 @@ pl = do
         let year = read yearString :: Int
         return $ MklongDate (D dayString monthString year) (read heureString :: Int) (read minuteString :: Int) (read secondeString :: Int)
 
+pl2 :: Parser LongDate
+pl2 = do
+        dayString <- many digit
+        char '/' <|> char '-'
+        monthString <- many digit
+        char '/' <|> char '-'
+        yearString <- many digit
+        char ' ' 
+        heureString <- many digit
+        char '/' <|> char '-'<|> char ':'
+        minuteString <- many digit
+        char '/' <|> char '-'<|> char ':'
+        secondeString <- many digit
+        let year = read yearString :: Int
+        return $ MklongDate (D dayString monthString year) (read heureString :: Int) (read minuteString :: Int) (read secondeString :: Int)
+
 parseLongDate :: String -> Either ParseError LongDate
 parseLongDate  = parse pl ""  
+
+parseLongDate2 :: String -> Either ParseError LongDate
+parseLongDate2  = parse pl2 ""  
 
 data Sms = MkSms {
         montantTransaction :: Int,
@@ -117,9 +137,52 @@ parseSMS :: String -> Either ParseError Sms
 parseSMS = parse ps ""
 
 
+--parser un message UBA--
+ 
+-- Txn: Debit Compte: 0XX..29X Valeur: XAF 500.00 Date: 31-10-2022 23:41:00 Solde: XAF 0.00 
+
+data Uba = MkUba {
+    typeTransaction :: String,
+    numeroCompte :: String,
+    valeur :: Double,
+    dateUba :: Either ParseError LongDate,
+    soldeUba :: Double
+} deriving Show
+
+pu :: Parser Uba
+pu = do
+    string "Txn: "
+    typeT <- many letter
+    string " Compte: "
+    numeroC <- many (letter <|> digit <|> char '.')
+    string " Valeur: XAF "
+    val <- many (digit <|> char '.')
+    string " Date: "
+    dattU <- many (digit <|> oneOf "-: ")
+    let datU2 = parseLongDate2 dattU
+    string "Solde: XAF "
+    soldeU <- many (digit <|> char '.')
+    return $ MkUba typeT numeroC (read val:: Double) datU2 (read soldeU :: Double)
 
 
 
+parseUba :: String -> Either ParseError Uba
+parseUba = parse pu ""
+
+
+parserEmail :: Parser String          -- jean94@domain.com
+parserEmail = do
+   first <- many lower
+   chiffre <- many (lower <|> digit)
+   char '@'
+   second <- many lower
+   char '.'
+   end <- many lower
+   let email = first ++ chiffre ++  "@" ++ second ++ "." ++ end
+   return email
+ 
+fonctionEmail :: String -> Either ParseError String
+fonctionEmail = parse parserEmail "email non valide"
 
 
 
