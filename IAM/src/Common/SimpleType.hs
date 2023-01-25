5c1bf8e7453a1bba2e92b6dd4237a0ef60224b4a
ntet2001 <manyamaigor2001@gmail.com>
2023-01-25 10:43:52 +0100

    Adding read insatnce of my sub module operator

--- IAM/src/Common/SimpleType.hs
+++ IAM/src/Common/SimpleType.hs
@@ -21,22 +21,7 @@ where
     type PrenomOp = Nom 
     type Matricule = String
     type Identifiant = String
-    data Email = MkEmail { identifiant :: Identifiant, domaine :: String, extension :: String } deriving Read
+    data Email = MkEmail { identifiant :: Identifiant, domaine :: String, extension :: String } deriving (Show ,Read)
     type PasswordOp = String
     type Photo = C.ByteString
-    data Operateur = MKOperateur { nomOp :: NomOp, prenomOp :: PrenomOp, matricule::Matricule, email :: Email, passwordOp :: PasswordOp, photo :: Photo } deriving Read 
-
-    --instance Show of Email
-    instance Show Email where
-        show y = if null (identifiant y)
-            then error "identifiant nul"
-            else identifiant y ++ "@" ++ domaine y ++ "." ++ extension y
-
-    -- instance Show of Operateur
-    instance Show Operateur where
-        show operateur = "{\"name\" : " ++ nomOp operateur ++ ";\n"++
-            "\"prenom\" : " ++ prenomOp operateur ++ ";\n"++
-            "\"matricule\" : " ++ matricule operateur ++ ";\n"++
-            "\"email\" : " ++ show (email operateur) ++ ";\n"++
-            "\"password\" : " ++ passwordOp operateur ++ ";\n" ++
-            "\"photo\" : " ++ C.unpack (photo operateur) ++ ";\n}"
+    data Operateur = MKOperateur { nomOp :: NomOp, prenomOp :: PrenomOp, matricule::Matricule, email :: Email, passwordOp :: PasswordOp, photo :: Photo } deriving (Show,Read) 
